34b0535cafb3f17a606180dc3f2477c9d8e8d78e
InnaShavarovska <innashavarovska@gmail.com>
2023-03-26 16:49:13 +0300

    search country

--- src/index.js
+++ src/index.js
@@ -9,11 +9,11 @@ const countryInput = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 
-countryInfo.addEventListener('input', debounce(onInputClick, DEBOUNCE_DELAY));
+countryInput.addEventListener('input', debounce(onInputClick, DEBOUNCE_DELAY));
 
 let searchName = '';
 
-function onInputClick(event) {
+function onInputClick() {
 
 	searchName = countryInput.value.trim();
 
@@ -23,16 +23,15 @@ function onInputClick(event) {
 		return;
 	} 
 	fetchCountries(searchName).then(countries =>  {
-	   if (countries.length > 1) {
-		  countryList.insertAdjacentHTML('beforeend', renderCountryList(countries));
-        countryInfo.insertAdjacentHTML('beforeend', renderCountryInfo(countries));
-	  } else if (countries.length > 10) {
-		countryInfo.innerHTML = '';
-		countryList.innerHTML = '';
-		Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-	  } else {
-		countryList.insertAdjacentHTML('beforeend', renderCountryList(countries));
-	  };
+		if (countries.length > 10) {
+			Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+		  } else if (countries.length >=2 && countries.length <= 10) {
+         	countryInfo.innerHTML = '';
+		      countryList.innerHTML = '';
+				countryList.insertAdjacentHTML('beforeend', renderCountryList(countries));
+		  } else {
+			countryInfo.insertAdjacentHTML('beforeend', renderCountryInfo(countries));
+		  }
  })
 	  .catch(() => {
 		countryInfo.innerHTML = '';
@@ -46,12 +45,16 @@ function renderCountryInfo(countries) {
 	countryInfo.innerHTML = '';
 	countryList.innerHTML = '';
 	
-	const readyCard = countries.map(({ capital, population, languages }) => {
+	const readyCard = countries.map(({ name, flags, capital, population, languages }) => {
 		return `
-		<ul class="country-card">
-			 <li class="country-card--item">Capital: <span class="country-value">${capital}</span></li>
-			 <li class="country-card--item">Population: <span class="country-value">${population}</span></li>
-			 <li class="country-card--item">Languages: <span class="country-value">${Object.values(languages).join(',')}</span></li>
+		<div class="country__flag">
+		<img class="country__img" src="${flags.svg}" alt="flag">
+		<p class="country__name">${name.official}</p>
+	 </div>
+		<ul class="country-info">
+			 <li class="country-info__item">Capital: <span class="country__value">${capital}</span></li>
+			 <li class="country-info__item">Population: <span class="country__value">${population}</span></li>
+			 <li class="country-info__item">Languages: <span class="country__value">${Object.values(languages).join(',')}</span></li>
   </ul>`
 	}) .join("");
 	return readyCard; 
@@ -61,9 +64,9 @@ function renderCountryList(countries) {
 	countryInfo.innerHTML = '';
 	countryList.innerHTML = '';
 	const readyList = countries.map(({ name, flags }) => {
-		return  `<li class="country-list--item">
-			  <img src="${flags.svg}" alt="Country flag" width="40", height="30">
-			  <span class="country-list--name">${name.official}</span>
+		return  `<li class="country-list__item">
+			  <img class="country-list__img" src="${flags.svg}" alt="Country flag" width="40", height="30">
+			  <p class="country-list__text">${name.official}</p>
 		 </li>`})
 		 .join("");
 	return readyList;
